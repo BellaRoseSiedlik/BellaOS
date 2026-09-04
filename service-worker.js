98fca95bd40e039a4d3b6f8593951210b7178dc5
BellaRoseSiedlik <izzysiedlik@gmail.com>
2026-09-04 00:02:11 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bellaos-preview-v1';
+const CACHE_NAME = 'bellaos-preview-v2';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
